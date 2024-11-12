d25750d4557c6e6e95e354feb1a31205b2550d85
yemou <dev@lilac.pink>
2024-11-12 18:34:14 -0500

    Initial Work on python implementation
    As more gets implemented, the C# codebase will be removed

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "CPU Scheduler GUI to Compare CPU Schedulers";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function: nixpkgs.lib.genAttrs [ "x86_64-linux" ]
+        (system: function (import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        }));
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hatch
+            python3
+            ruff
+            (with python3Packages; [
+              build
+              hatchling
+            ])
+          ];
+        };
+      });
+
+      overlays.default = final: prev: {
+        cpu-schedulers = final.callPackage ./nix/packages { };
+      };
+
+      packages = forAllSystems (pkgs: rec {
+        default = cpu-schedulers;
+        cpu-schedulers = pkgs.cpu-schedulers;
+      });
+
+      apps = forAllSystems (pkgs: rec {
+        default = cpu-schedulers;
+        cpu-schedulers = {
+          type = "app";
+          program = "${pkgs.cpu-schedulers}/bin/cpu-schedulers";
+        };
+      });
+    };
+}
---
+++ nix/packages/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib }:
+pkgs.python3Packages.buildPythonApplication {
+  pname = "CPU-Schedulers";
+  version = "0.0.1";
+  pyproject = true;
+
+  src = ../../.;
+
+  build-system = with pkgs.python3Packages; [ hatchling ];
+  # dependencies = [ ];
+  # nativeCheckInputs = [ ];
+
+  meta = {
+    description = "CPU Scheduler GUI to Compare CPU Schedulers";
+    homepage = "https://github.com/yemouu/CPU-Simulator-GUI";
+    license = lib.licenses.mit;
+  };
+}
