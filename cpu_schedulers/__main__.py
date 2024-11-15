2c81154ce12dd5ea72b6b73115eebb087c272c99
yemou <dev@lilac.pink>
2024-11-15 14:49:14 -0500

    Move from process names to process IDs

--- cpu_schedulers/__main__.py
+++ cpu_schedulers/__main__.py
@@ -4,14 +4,14 @@ from .process import Process
 
 def main() -> None:
     processes = [
-        Process(0, 5, "1"),
-        Process(1, 2, "2"),
-        Process(2, 2, "3"),
-        Process(3, 1, "4"),
-        Process(3, 1, "5"),
-        Process(5, 5, "6"),
-        Process(10, 4, "7,"),
-        Process(10, 4, "8,"),
+        Process(0, 5),
+        Process(1, 2),
+        Process(2, 2),
+        Process(3, 1),
+        Process(3, 1),
+        Process(5, 5),
+        Process(10, 4),
+        Process(10, 4),
     ]
 
     fcfs = FirstComeFirstServe(processes)
--- cpu_schedulers/process.py
+++ cpu_schedulers/process.py
@@ -1,16 +1,20 @@
 class Process:
-    def __init__(self, arrival_time: int, duration: int, name: str | None = None):
+    _next_id: int = 0
+
+    def __init__(self, arrival_time: int, duration: int):
         self._arrival_time: int = arrival_time
-        self._name: str = name
         self._time_left: int = duration
 
         self._done: bool = False
+
+        self._id: int = Process._next_id
+        Process._next_id += 1
 
         if self._time_left < 1:
             raise ValueError("Duration is less than 1")
 
     def __str__(self):
-        return f"Process {self._name}" if self._name else super().__str__
+        return f"pid-{self._id}"
 
     @property
     def arrival_time(self) -> int:
