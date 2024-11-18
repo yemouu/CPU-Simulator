26c8cccc857fdca28366c25d8704ca118d119a91
yemou <dev@lilac.pink>
2024-11-18 02:01:57 -0500

    Update main with an example to showcase deadlock avoidance

--- cpu_schedulers/__main__.py
+++ cpu_schedulers/__main__.py
@@ -1,93 +1,53 @@
+# TODO: Add a repl or something to allow the user to select the algorithm and the amount of processes and to customize
+#     processes
+# TODO: Create a function to randomly (seeded) generate a list of n processes **
+# TODO: Create another function that will for sure create a deadlock(? I may not need to do this, I can just reduce
+#     the amount of resources in the system and it should happen naturally)
+# TODO: Collect Metrics **
+# TODO: Generate a Gantt Chart?
+# TODO: Generate Graphs
+
 from .fcfs import FirstComeFirstServe
 from .process import Process, Task, TaskAction
 from .resource import Resource
-from .resource_manager import ResourceManager
+from .resource_manager import AvoidantResourceAllocationGraph, ResourceAllocationGraph, ResourceManager
 from .round_robin import RoundRobin
 
 
 def main() -> None:
-    resource_manager: ResourceManager = ResourceManager((Resource(),))
+    resource_manager: ResourceManager = AvoidantResourceAllocationGraph((Resource(), Resource()))
     processes_1: list[Process] = [
         Process(
             0,
-            5,
+            6,
             [
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 0),
+                Task(TaskAction.IO, 1),
                 Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 1),
+                Task(TaskAction.IO, 1),
+                Task(TaskAction.RESOURCE_RELEASE, 1),
+                Task(TaskAction.IO, 1),
+                Task(TaskAction.RESOURCE_RELEASE, 0),
                 Task(TaskAction.NONE, -1),
             ],
         ),
         Process(
-            1,
-            2,
+            0,
+            6,
             [
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 1),
+                Task(TaskAction.IO, 1),
                 Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 0),
+                Task(TaskAction.IO, 1),
+                Task(TaskAction.RESOURCE_RELEASE, 1),
+                Task(TaskAction.RESOURCE_REQUEST, 0),
+                Task(TaskAction.IO, 1),
                 Task(TaskAction.NONE, -1),
             ],
         ),
-        Process(
-            2,
-            2,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            3,
-            1,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            3,
-            1,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            5,
-            5,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            10,
-            4,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            10,
-            4,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
+        Process(0, 6, []),
     ]
 
     fcfs = FirstComeFirstServe(processes_1, resource_manager)
@@ -100,84 +60,35 @@ def main() -> None:
     processes_2: list[Process] = [
         Process(
             0,
-            5,
+            6,
             [
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 0),
+                Task(TaskAction.IO, 1),
                 Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 1),
+                Task(TaskAction.IO, 1),
+                Task(TaskAction.RESOURCE_RELEASE, 1),
+                Task(TaskAction.IO, 1),
+                Task(TaskAction.RESOURCE_RELEASE, 0),
                 Task(TaskAction.NONE, -1),
             ],
         ),
         Process(
-            1,
-            2,
+            0,
+            6,
             [
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 1),
+                Task(TaskAction.IO, 1),
                 Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
+                Task(TaskAction.RESOURCE_REQUEST, 0),
+                Task(TaskAction.IO, 1),
+                Task(TaskAction.RESOURCE_RELEASE, 1),
+                Task(TaskAction.RESOURCE_REQUEST, 0),
+                Task(TaskAction.IO, 1),
                 Task(TaskAction.NONE, -1),
             ],
         ),
-        Process(
-            2,
-            2,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            3,
-            1,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            3,
-            1,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            5,
-            5,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            10,
-            4,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
-        Process(
-            10,
-            4,
-            [
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-                Task(TaskAction.IO, 2),
-                Task(TaskAction.NONE, -1),
-            ],
-        ),
+        Process(0, 6, []),
     ]
 
     round_robin = RoundRobin(processes_2, resource_manager, 2)
